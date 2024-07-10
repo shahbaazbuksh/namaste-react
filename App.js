ac6186ece6a7500fcf35c1e0dba580f7eb7ca826
Shahbaaz Buksh <shahbaazbuksh@yahoo.com>
2024-07-10 23:33:06 +0530

    Chapter 3: Laying the foundation

--- App.js
+++ App.js
@@ -1,43 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-
-// heading is an object created by createElement
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("p", {}, "I am a p tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
-
-// If we want to create sibling elements we have to write it inside an array
-
-/*
-
-In order to create this below structure we have to write above code
-
-<div id="parent">
-   <div id="child1">
-      <h1>I am an h1 tag</h1>
-      <h2>I am an h2 tag</h2>
-   </div>
-   <div id="child2">
-      <h1>I am an h1 tag</h1>
-      <h2>I am an h2 tag</h2>
-   </div>
-</div>
-
-*/
+function HeadingComponent() {
+  return <h1 id="container">Namaste React from Component</h1>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);

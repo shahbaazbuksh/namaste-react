10282a647dc866e885d081e7bf887c963482ce84
Shahbaaz Buksh <shahbaazbuksh@yahoo.com>
2024-07-19 05:46:27 +0530

    Chapter 9: Optimising our app

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
@@ -26,6 +27,10 @@ const Body = () => {
       json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
   };
+
+  if (useOnlineStatus() === false)
+    return <h1>Looks like your internet connection is down!!!</h1>;
+  // console.log(useOnlineStatus());
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+function Grocery() {
+  return (
+    <div>
+      <h1>It's a very big grocery store</h1>
+    </div>
+  );
+}
+
+export default Grocery;

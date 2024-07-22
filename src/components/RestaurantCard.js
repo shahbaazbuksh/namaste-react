ddc546317f571b6d07e773bb067ef41780cab251
Shahbaaz Buksh <shahbaazbuksh@yahoo.com>
2024-07-22 23:44:24 +0530

    Chapter 10: Jo dikhta hai, wo bikta hai

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,9 +13,9 @@ export default function RestaurantCard({ resData }) {
     sla: { deliveryTime },
   } = resData.info;
   return (
-    <div className="res-card" style={styleCard}>
-      <img className="res-logo" src={`${CDN_URL}${cloudinaryImageId}`} />
-      <h3>{name}</h3>
+    <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
+      <img className="rounded-lg" src={`${CDN_URL}${cloudinaryImageId}`} />
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{deliveryTime} minutes</h4>
--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,6 @@
 function User() {
   return (
-    <div className="user-card">
+    <div className="user-card m-4 p-4 bg-gray-50 rounded-lg">
       <h2>Name: Shahbaaz</h2>
       <h3>Location: Kolkata</h3>
       <h4>Contact: @shahbaaz</h4>
